12de36132ce43c607fc81ad752392c0f5b45c56f
Gabriel Tuco <gabriel_tuco@hotmail.com>
2024-07-17 15:24:14 -0500

    Final version of NMT

--- models/nmt_prior.py
+++ models/nmt_prior.py
@@ -141,9 +141,9 @@ if __name__ == "__main__":
     # _config["cores"] = 0
     # _config["pin_memory"] = False
     print(_config)
-    # trained_model = run(_config)
+    trained_model = run(_config)
 
-    # eval_best(trained_model)
+    eval_best(trained_model)
 
     if "backtranslate_path" in trained_model.config["data"]:
         backtranslate(trained_model)
